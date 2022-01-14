7c60f44dacfe27f06a112b2fa4a06d7be5314cbd
aXlireza <alireza.ataee01@gmail.com>
2022-01-14 18:16:15 +0330

    Architect modification and preparing funcrions

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-const svgjson = require('svgjson');
+const ttfTables = require('./lib/font')
 
-const GSUB = require('./lib/font/gsub')
-const OS2 = require('./lib/font/os2')
-const CMAP = require('./lib/font/cmap')
-const GLYPH = require('./lib/font/glyph')
+module.exports = {
+  ttfTables,
+}
