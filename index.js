f5975ebd307e1bf90e98089b34c9e90144ad9911
aXlireza <alireza.ataee01@gmail.com>
2022-01-14 14:34:13 +0330

    arranging the files architecture

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const svgjson = require('svgjson');
 
-const bufferHandler = require('./lib/bufferHandler')
 const GSUB = require('./lib/gsub')
 const OS2 = require('./lib/os2')
 const CMap = require('./lib/cmap')
