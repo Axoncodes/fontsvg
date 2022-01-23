909f60c0eccb3fda30a3995ec91a9b128323b685
aXlireza <devops@axoncodes.com>
2022-01-23 09:56:02 +0330

    Modification and using the svg2ttf module

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-const ttfTables = require('./lib/font')
+const svgJson = require('../svgjson');
+const svg2ttf = require('svg2ttf');
+const fs = require('fs');
 
-module.exports = {
-  ttfTables,
+/**
+ *  
+ * @param {string} svgFile If it's an svg file you want to parse to ttf file, fillout this parameter
+ * @param {string} fontsvgFile Otherwise, it would be a fontsvgFile requsted to parse to ttf file
+ * @param {string} filename The name of the file for the ttf font to be stored in
+ * @retuen returnes the ttf file
+ */
+function parseTTF(opt) {
+  return handleFile(opt)
+  .then(svg2ttf)
+  .then(ttf => Buffer.from(ttf.buffer))
+  .then(ttf => fs.writeFileSync(opt.filename, ttf))
 }
+
+async function handleFile(opt) {
+  // info handling
+  const { svgFile, fontsvgFile, filename } = opt
+  if((!svgFile && !fontsvgFile) || !filename) throw 'ERROR: No Input file was provided'
+
+  // read the file (whether svg or fontsvg)
+  const input = fs.readFileSync(svgFile || fontsvgFile, 'utf8')
+  // if the parameter fontsvgFile was availabel, then just return the file content
+  if(fontsvgFile) return input
+  // otherwise, convert the svg file to fontsvg and then return
+  const fontsvg = await svgJson.convert({ outputFormat: 'fontsvg', input })
+  // write the fontsvg file
+  fs.writeFileSync('./testfont.svg', fontsvg)
+  return fontsvg
+}
+
+module.exports = parseTTF;
