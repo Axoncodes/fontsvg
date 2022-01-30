4d653f913f5017aed7d65d35a7b229767ce6aec8
aXlireza <devops@axoncodes.com>
2022-01-30 11:20:27 +0330

    Implementing more fontface src options for browser matching

--- src/style/fontface.js
+++ src/style/fontface.js
@@ -5,7 +5,12 @@ function fontfaceHandler(fontface) {
   let fileContent = '';
   fileContent += `@font-face {\n`
   fileContent += `font-family: "${fontface.attributes['font-family']}";\n`;
-  fileContent += `src: url('./font.ttf');\n`;
+  fileContent += `src: url('./font.eot'); /* IE9 Compat Modes */\n`;
+  fileContent += `src: url('./font.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */\n`;
+  fileContent += `url('./font.woff2') format('woff2'), /* Super Modern Browsers */\n`;
+  fileContent += `url('./font.woff') format('woff'), /* Pretty Modern Browsers */\n`;
+  fileContent += `url('./font.ttf')  format('truetype'), /* Safari, Android, iOS */\n`;
+  fileContent += `url('./fontsvg.svg#ifont') format('svg'); /* Legacy iOS */\n`;
   fileContent += `}\n`;
   return fileContent;
 }
