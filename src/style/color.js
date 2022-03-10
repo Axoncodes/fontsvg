32e7c3d50325194f01cc5be76f4623bd74f16f78
aXoncodes <devops@axoncodes.com>
2022-03-11 02:39:17 +0330

    style color and opacity

--- src/style/color.js
+++ src/style/color.js
@@ -2,10 +2,9 @@ const xjs = require('@axoncodes/xjs')
 
 function colorHandler(glyph) {
   let subfileContent = '';
-  if (xjs.if(glyph, 'glyph.attributes.opacity') || xjs.if(glyph, 'glyph.attributes.color')) {
-    subfileContent += xjs.if(glyph, 'glyph.attributes.opacity') ? `opacity: ${glyph.attributes.opacity};` : '';
-    subfileContent += xjs.if(glyph, 'glyph.attributes.fill') || xjs.if(glyph, 'glyph.attributes.color') ? `color: ${glyph.attributes.color || glyph.attributes.fill};` : (xjs.if(glyph, 'glyph.attributes.opacity') ? 'color: #000;' : '');
-  }
+  subfileContent += xjs.if(glyph, 'glyph.attributes.opacity') ? `opacity: ${glyph.attributes.opacity};` : '';
+  subfileContent += xjs.if(glyph, 'glyph.attributes.fill') || xjs.if(glyph, 'glyph.attributes.color')
+    ? `color: ${glyph.attributes.color || glyph.attributes.fill};` : '';
   return subfileContent;
 }
 
