236e744779df252c464a175a0a089272b0b701d6
aXoncodes <devops@axoncodes.com>
2022-03-10 14:28:39 +0330

    cleanup

--- helpers/tools.js
+++ helpers/tools.js
@@ -1,12 +1,3 @@
-const fs = require('fs')
-const svgJson = require('svgjson')
-
-function readFiles(files) {
-  return Promise.all(files.map(file => 
-    svgJson.parseJson(fs.readFileSync(file, 'utf8'))
-  ))
-}
-
 function extractFontface(json) {
   let fontface;
   json.forEach(tag => {
@@ -17,5 +8,4 @@ function extractFontface(json) {
 
 module.exports = {
   extractFontface,
-  readFiles,
 }
