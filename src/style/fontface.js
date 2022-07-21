07fc7ebf91a4b8e9bdda104cd41aec82bf19c867
Axoncodes <devops@axoncodes.com>
2022-07-21 07:15:35 +0430

    add prefix option

--- src/style/fontface.js
+++ src/style/fontface.js
@@ -1,13 +1,13 @@
-function fontfaceHandler(fontface, suffix, weight) {
+function fontfaceHandler(fontface, suffix, weight, prefix=".") {
   let fileContent = '';
   fileContent += `@font-face {\n`
   fileContent += `font-family: "${fontface.attributes['font-family']}";\n`;
-  fileContent += `src: url('./font${suffix}.eot'); /* IE9 Compat Modes */\n`;
-  fileContent += `src: url('./font${suffix}.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */\n`;
-  // fileContent += `url('./font${suffix}.woff2') format('woff2'), /* Super Modern Browsers */\n`;
-  fileContent += `url('./font${suffix}.woff') format('woff'), /* Pretty Modern Browsers */\n`;
-  fileContent += `url('./font${suffix}.ttf')  format('truetype'), /* Safari, Android, iOS */\n`;
-  fileContent += `url('./fontsvg${suffix}.svg#ifont') format('svg'); /* Legacy iOS */\n`;
+  fileContent += `src: url('${prefix}/font${suffix}.eot'); /* IE9 Compat Modes */\n`;
+  fileContent += `src: url('${prefix}/font${suffix}.eot?#iefix') format('embedded-opentype'), /* IE6-IE8 */\n`;
+  // fileContent += `url('${prefix}/font${suffix}.woff2') format('woff2'), /* Super Modern Browsers */\n`;
+  fileContent += `url('${prefix}/font${suffix}.woff') format('woff'), /* Pretty Modern Browsers */\n`;
+  fileContent += `url('${prefix}/font${suffix}.ttf')  format('truetype'), /* Safari, Android, iOS */\n`;
+  fileContent += `url('${prefix}/fontsvg${suffix}.svg#ifont') format('svg'); /* Legacy iOS */\n`;
   fileContent += `font-weight: ${weight};\n`;
   fileContent += `}\n`;
   return fileContent;
